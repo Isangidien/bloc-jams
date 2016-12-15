d84d8440176e7e617f1f567f551bf54d1fd8b558
Inessa Miller <i.inessa.miller@gmail.com>
2016-12-14 19:55:25 -0500

    Checkpoint 19 nextPrevious: completed

---
+++ scripts/fixtures.js
@@ -0,0 +1,45 @@
+var albumPicasso = {
+    title: 'The Colors',
+    artist: 'Pablo Picasso',
+    label: 'Cubism',
+    year: '1881',
+    albumArtUrl: 'assets/images/album_covers/01.png',
+    songs: [
+        { title: 'Blue', duration: '4:26' },
+        { title: 'Green', duration: '3:14' },
+        { title: 'Red', duration: '5:01' },
+        { title: 'Pink', duration: '3:21' },
+        { title: 'Magenta', duration: '2:15' }
+    ]
+};
+
+ var albumMarconi = {
+     title: 'The Telephone',
+     artist: 'Guglielmo Marconi',
+     label: 'EM',
+     year: '1909',
+     albumArtUrl: 'assets/images/album_covers/20.png',
+     songs: [
+         { title: 'Hello, Operator?', duration: '1:01' },
+         { title: 'Ring, ring, ring', duration: '5:01' },
+         { title: 'Fits in your pocket', duration: '3:21'},
+         { title: 'Can you hear me now?', duration: '3:14' },
+         { title: 'Wrong phone number', duration: '2:15'}
+     ]
+ };
+
+//My pick Album
+var albumLewis = {
+    title: 'Trilogy + 2',
+    artist: 'Clive Staples Lewis',
+    label: 'Scifi',
+    year: '1938',
+    albumArtUrl: 'assets/images/album_covers/13.png',
+    songs: [
+        { title: 'Out of the Silent Planet', duration: '5:26' },
+        { title: 'Perelandra', duration: '4:14' },
+        { title: 'That Hideous Strength', duration: '6:01' },
+        { title: 'The Tortured Planet', duration: '4:21'},
+        { title: 'The Dark Tower', duration: '3:15'}
+    ]		          
+};
